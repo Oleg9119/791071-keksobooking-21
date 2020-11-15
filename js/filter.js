4f3338e46e8c25fff6a6b8a1228a5eec4d79795f
Олег Котляров <o.kotlyarov@r-service.net>
2020-11-15 21:12:55 +0300

    фильтрация - часть 2

--- js/filter.js
+++ js/filter.js
@@ -1,9 +1,76 @@
 'use strict';
 
 (() => {
-  const housingTypeFilter = document.querySelector(`#housing-type`);
+  const MIN_PRICE = 10000;
+  const MAX_PRICE = 50000;
 
-  let chosenHousingType = `any`;
+  const mapFilters = document.querySelector(`.map__filters`);
+  const housingTypeFilter = mapFilters.querySelector(`#housing-type`);
+
+  const housingPriceFilter = mapFilters.querySelector(`#housing-price`);
+  const housingRoomsFilter = mapFilters.querySelector(`#housing-rooms`);
+  const housingGuestsFilter = mapFilters.querySelector(`#housing-guests`);
+
+  const housingFeaturesFilter = mapFilters.querySelector(`#housing-features`);
+  const housingFeaturesWiFiFilter = housingFeaturesFilter.querySelector(`#filter-wifi`);
+  const housingFeaturesDishwasherFilter = housingFeaturesFilter.querySelector(`#filter-dishwasher`);
+  const housingFeaturesParkingFilter = housingFeaturesFilter.querySelector(`#filter-parking`);
+  const housingFeaturesWasherFilter = housingFeaturesFilter.querySelector(`#filter-washer`);
+  const housingFeaturesElevatorFilter = housingFeaturesFilter.querySelector(`#filter-elevator`);
+  const housingFeaturesConditionerFilter = housingFeaturesFilter.querySelector(`#filter-conditioner`);
+
+  const chosenFilters = {
+    type: `any`,
+    price: `any`,
+    rooms: `any`,
+    guests: `any`,
+    features: new Set()
+  };
+
+  const elementsToFiltersMap = {
+    'housing-type': `type`,
+    'housing-price': `price`,
+    'housing-rooms': `rooms`,
+    'housing-guests': `guests`,
+    'filter-wifi': `wifi`,
+    'filter-dishwasher': `dishwasher`,
+    'filter-parking': `parking`,
+    'filter-washer': `washer`,
+    'filter-elevator': `elevator`,
+    'filter-conditioner': `conditioner`
+  };
+
+  const checkFilterAds = (filterType, filterData, filterCondition) => {
+    return (filterType === filterCondition || filterData.toString() === filterType);
+  };
+
+  const checkPrice = (filterType, filterData) => {
+    switch (filterType) {
+      case `any`:
+        return true;
+      case `low`:
+        return filterData < MIN_PRICE;
+      case `middle`:
+        return filterData >= MIN_PRICE && filterData < MAX_PRICE;
+      case `high`:
+        return filterData >= MAX_PRICE;
+      default:
+        return false;
+    }
+  };
+
+  const checkChosenFeatures = (array, feature) => {
+    if (array.has(feature)) {
+      array.delete(feature);
+    } else {
+      array.add(feature);
+    }
+  };
+
+  const checkFeatures = (filterType, filterData) => {
+    const checkedFeatures = [...filterType];
+    return checkedFeatures.every((elem) => filterData.includes(elem));
+  };
 
   const createFilteredAds = () => {
     window.map.removePins();
@@ -16,17 +83,39 @@
       if (filteredAds.length >= 5) {
         break;
       }
-      if (chosenHousingType === `any` || allAds[i].offer.type === chosenHousingType) {
+      if (checkFilterAds(chosenFilters.type, allAds[i].offer.type, `any`) &&
+        checkFilterAds(chosenFilters.rooms, allAds[i].offer.rooms, `any`) &&
+        checkFilterAds(chosenFilters.guests, allAds[i].offer.guests, `any`) &&
+        checkPrice(chosenFilters.price, allAds[i].offer.price) &&
+        checkFeatures(chosenFilters.features, allAds[i].offer.features)) {
         filteredAds.push(allAds[i]);
       }
     }
     window.map.createAds(filteredAds);
   };
 
-  housingTypeFilter.addEventListener(`change`, (evt) => {
-    evt.preventDefault();
-    chosenHousingType = evt.target.value;
+  const onSelectChange = (evt) => {
+    const filterName = elementsToFiltersMap[evt.target.name];
+    chosenFilters[filterName] = evt.target.value;
+    window.debounce(createFilteredAds);
+  };
 
-    createFilteredAds();
-  });
+  const onCheckboxChange = (evt) => {
+    const filterName = elementsToFiltersMap[evt.target.id];
+    chosenFilters[filterName] = evt.target.id;
+    checkChosenFeatures(chosenFilters.features, filterName);
+    window.debounce(createFilteredAds);
+  };
+
+  housingTypeFilter.addEventListener(`change`, onSelectChange);
+  housingPriceFilter.addEventListener(`change`, onSelectChange);
+  housingRoomsFilter.addEventListener(`change`, onSelectChange);
+  housingGuestsFilter.addEventListener(`change`, onSelectChange);
+
+  housingFeaturesWiFiFilter.addEventListener(`change`, onCheckboxChange);
+  housingFeaturesDishwasherFilter.addEventListener(`change`, onCheckboxChange);
+  housingFeaturesParkingFilter.addEventListener(`change`, onCheckboxChange);
+  housingFeaturesWasherFilter.addEventListener(`change`, onCheckboxChange);
+  housingFeaturesElevatorFilter.addEventListener(`change`, onCheckboxChange);
+  housingFeaturesConditionerFilter.addEventListener(`change`, onCheckboxChange);
 })();
